6591333f47de6b50b149f1b1bec4813c6ac1a4fa
Marcel-star-crtl <marcelngong50@gmail.com>
2023-10-29 03:51:19 +0100

    Checkout

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    email: { 
+      type: String, 
+      required: true, 
+      unique: true, 
+      match: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    },
     password: { type: String, required: true },
     isAdmin: {
       type: Boolean,
